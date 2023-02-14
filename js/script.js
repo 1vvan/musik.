8503a2920492bd79bf0d3a78d2fcd4ba151da972
Ivan <123951392+1vvan@users.noreply.github.com>
2023-02-14 11:06:24 +0200

    Musik.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Burger Menu
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("header").classList.toggle("open");
+    })
+})
